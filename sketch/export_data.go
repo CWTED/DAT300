d20db31795f47fdc88fbc11d1459e4dc8ce6665a
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2023-10-01 19:16:33 +0200

    Add Data struct which consists of Packet, observed and threshold change

--- sketch/export_data.go
+++ sketch/export_data.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/csv"
-	"os"
 	"log"
-	"github.com/barweiss/go-tuple"
+	"os"
+	"strconv"
+
+	"sketch/datastream"
 )
 
-func ExportData(channel chan tuple.T4[string,string,string,string]) {
+func ExportData(channel chan Data) {
 	f, err := os.OpenFile("data.csv", os.O_RDWR|os.O_CREATE, 0755)
 	if err != nil {
 		log.Fatal(err)
@@ -23,11 +25,17 @@ func ExportData(channel chan tuple.T4[string,string,string,string]) {
 			break
 		}
 		
-		a, b, c, d := data.Values()
-		row := []string{a, b, c, d}
+		row := []string{data.packet.SrcIP, data.packet.DstIP, data.packet.SrcPort, data.packet.DstPort, 
+						data.packet.Protocol, strconv.FormatUint(data.observedChange, 10), strconv.FormatUint(data.thresholdChange, 10)}
 		if err := w.Write(row); err != nil {
 			log.Fatalln("error writing tuple to file", err)
 		}
 		w.Flush()
 	}
+}
+
+type Data struct {
+	packet datastream.Packet
+	observedChange uint64
+	thresholdChange uint64
 }
