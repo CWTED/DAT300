fe0be9cc6250d0bc339a9d1cc2f441e6d73b933c
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2023-10-01 19:22:51 +0200

    Add change detection and export of data to data.csv

--- sketch/main.go
+++ sketch/main.go
@@ -9,6 +9,7 @@ import (
 	"sketch/datastream"
 	"sketch/sketch"
 	"sketch/forecasting"
+	"sketch/change"
 )
 
 func main() {
@@ -25,6 +26,8 @@ func main() {
 		log.Fatalln("program usage: kary <pcap file> <# hash rows> <# elements in a hash row>", err)
 	}
 
+	anomalies := make(chan Data)
+	go ExportData(anomalies)
 
 	receivedList := datastream.StreamData(os.Args[1]) // data pre-processing
 
@@ -37,13 +40,16 @@ func main() {
 	// Forcasting variables
 	var (
 		forecastAlgo forecasting.Forecasting
-		fSketch sketch.Sketch
+		fSketch *sketch.Sketch
 	)
-	forecastAlgo = forecasting.EWMA{Alpha: 0.5}
+	forecastAlgo = forecasting.EWMA{Alpha: 0.1}
+
+	// Variable representing how many packets is one epoch
+	epoch := 10
 
 	for index, packet := range receivedList {
 		// Update the forecasting sketch if there is a new epoch
-		if index % 10 == 0 {
+		if index % epoch == 0 && index > 0 {
 			fSketch, err = forecastAlgo.Forecast(s)
 			if err != nil {
 				log.Fatalln("error while forcasting", err)
@@ -55,7 +61,19 @@ func main() {
 
 		// Update the sketch with the incoming packet
 		s.Update(packet.ToBytes(), 1)
+
+		// Change detection
+		if index > epoch {
+			observedChange, thresholdChange, err := change.FEDetect(s, fSketch, 0.001, packet.ToBytes())
+			// If there is a change, save it in data.csv
+			if err != nil {
+				anomalies <- Data{packet: packet, observedChange: observedChange, thresholdChange: thresholdChange}
+				fmt.Printf("Anomaly detected!\t Observed change: %d, Threshold: %d", observedChange, thresholdChange)
+			}
+		}
+
 	}
 
+	fmt.Println("Main sketch at the end:")
 	s.Print()
 }
