5ea4727608f05148c5ab21e93e89f84c48de3539
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2023-10-02 20:33:34 +0200

    Add usage of flags and put kary into separate file

---
+++ sketch/kary.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"sketch/datastream"
+	"sketch/sketch"
+	"sketch/forecasting"
+	"sketch/change"
+)
+
+func Kary(file string, h int, k int, epoch int, threshold float64, alpha float64) {
+	anomalies := make(chan Data)
+	go ExportData(anomalies)
+	defer close(anomalies)
+
+	dataChannel := make(chan datastream.Packet)
+	go datastream.StreamData(file, dataChannel) // data pre-processing
+
+	// Create the main sketch
+	s, err := sketch.New(h, k)
+	if err != nil {
+		log.Fatalln("error while creating sketch", err)
+	}
+
+	// Forcasting variables
+	var (
+		forecastAlgo forecasting.Forecasting
+		fSketch *sketch.Sketch
+	)
+	forecastAlgo = &forecasting.EWMA{Alpha: alpha}
+
+
+	// Variable representing how many times the algorithm has iterated
+	index := 0
+
+	for packet := range dataChannel {
+		// Update the forecasting sketch if there is a new epoch
+		if index % epoch == 0 && index > 0 {
+			fSketch, err = forecastAlgo.Forecast(s)
+			if err != nil {
+				log.Fatalln("error while forcasting", err)
+			}
+		}
+
+		// Update the sketch with the incoming packet
+		s.Update(packet.ToBytes(), 1)
+
+		// Change detection
+		if index > epoch {
+			observedChange, thresholdChange, err := change.FEDetect(s, fSketch, threshold, packet.ToBytes())
+			// If there is a change, save it in data.csv
+			if err != nil {
+				anomalies <- Data{packet: packet, observedChange: observedChange, thresholdChange: thresholdChange}
+				fmt.Printf("Anomaly detected! Index: %d\t Observed change: %f, Threshold: %f\n", index, observedChange, thresholdChange)
+			}
+		}
+		index++
+	}
+
+}
--- sketch/main.go
+++ sketch/main.go
@@ -1,80 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"strconv"
-
-	"sketch/datastream"
-	"sketch/sketch"
-	"sketch/forecasting"
-	"sketch/change"
+	"flag"
 )
 
 func main() {
-	// Check if file argument is provided
-	if len(os.Args) < 4 {
-		log.Fatalln("program usage: kary <pcap file> <# hash rows> <# elements in a hash row>")
-	}
-	h, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		log.Fatalln("program usage: kary <pcap file> <# hash rows> <# elements in a hash row>", err)
-	}
-	k, err := strconv.Atoi(os.Args[3]) 
-	if err != nil {
-		log.Fatalln("program usage: kary <pcap file> <# hash rows> <# elements in a hash row>", err)
-	}
-
-	anomalies := make(chan Data)
-	go ExportData(anomalies)
-	defer close(anomalies)
-
-	dataChannel := make(chan datastream.Packet)
-	go datastream.StreamData(os.Args[1], dataChannel) // data pre-processing
-
-	// Create the main sketch
-	s, err := sketch.New(h, k)
-	if err != nil {
-		log.Fatalln("error while creating sketch", err)
-	}
-
-	// Forcasting variables
 	var (
-		forecastAlgo forecasting.Forecasting
-		fSketch *sketch.Sketch
+		file string
+		h int
+		k int 
+		epoch int
+		threshold float64
+		alpha float64
 	)
-	forecastAlgo = &forecasting.EWMA{Alpha: 0.1}
 
-	// Variable representing how many times the algorithm has iterated
-	index := 0
-	// Variable representing how many packets is one epoch
-	epoch := 1000
+	flag.StringVar(&file, "file", "", "pcap file location")
+	flag.IntVar(&h, "h", 5, "# of hash rows")
+	flag.IntVar(&k, "k", 8192, "# of elements in each hash array")
+	flag.IntVar(&epoch, "epoch", 1000, "# of packets in one epoch")
+	flag.Float64Var(&threshold, "threshold", 0.25, "Change threshold")
+	flag.Float64Var(&alpha, "alpha", 0.5, "Alpha used in EWMA")
 
-	for packet := range dataChannel {
-		// Update the forecasting sketch if there is a new epoch
-		if index % epoch == 0 && index > 0 {
-			fSketch, err = forecastAlgo.Forecast(s)
-			if err != nil {
-				log.Fatalln("error while forcasting", err)
-			}
-			//fSketch.Print()
-		}
+	flag.Parse()
 
-		// Update the sketch with the incoming packet
-		s.Update(packet.ToBytes(), 1)
-		//fmt.Println(s.EstimateF2())
-		// Change detection
-		if index > epoch {
-			observedChange, thresholdChange, err := change.FEDetect(s, fSketch, 0.3, packet.ToBytes())
-			// If there is a change, save it in data.csv
-			if err != nil {
-				anomalies <- Data{packet: packet, observedChange: observedChange, thresholdChange: thresholdChange}
-				fmt.Printf("Anomaly detected! Index: %d\t Observed change: %f, Threshold: %f\n", index, observedChange, thresholdChange)
-			}
-
-
-		}
-		index++
+	if file == "" {
+		log.Fatalln("error: location of pcap file needed")
 	}
+
+	Kary(file, h, k, epoch, threshold, alpha)
 }
