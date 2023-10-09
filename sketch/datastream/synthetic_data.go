20f30f5acfde6531d8f86eaa4fede4ce4661c792
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2023-10-09 02:37:06 +0200

    Add second packet

--- sketch/datastream/synthetic_data.go
+++ sketch/datastream/synthetic_data.go
@@ -5,10 +5,13 @@ import (
 )
 
 var p_A Packet = Packet{SrcIP: "1.1.1.1", SrcPort: "22", DstIP: "2.2.2.2", DstPort: "22", Protocol: "tcp"}
+var p_B Packet = Packet{SrcIP: "2.2.2.2", SrcPort: "22", DstIP: "1.1.1.1", DstPort: "22", Protocol: "tcp"}
 
 func SyntheticDataStream(channel chan Packet) {
-	for i := 0; i < 100_000; i++ {
+	for i := 0; i < 500_000; i++ {
 		channel <- p_A
 	}	
+	//channel <- p_B
+	//channel <- p_B
 	close(channel)
 }
