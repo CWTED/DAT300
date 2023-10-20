6adba78bc7679f8b3c7606eeda30cf3ff714096d
Willem Brahmstaedt <WilBra@users.noreply.github.com>
2023-10-20 16:41:22 +0200

    Make kary flexible for both forecasting algo

--- sketch/kary.go
+++ sketch/kary.go
@@ -10,13 +10,13 @@ import (
 	"sketch/sketch"
 )
 
-func Kary(file string, h int, k int, epoch int, threshold float64, alpha float64, datapoints int, method string) {
+func Kary(file string, h int, k int, epoch int, threshold float64, alpha float64, datapoints int, method string, forecasting_algo string) {
+	// Create channel for exporting anomalies
 	anomalies := make(chan Data)
-	var packetList []datastream.Packet
-	var anomCounter int
 	go ExportData(anomalies)
 	defer close(anomalies)
 
+	// Create channel for incoming packets
 	dataChannel := make(chan datastream.Packet)
 	if file != "" {
 		go datastream.StreamData(file, dataChannel) // data pre-processing
@@ -24,61 +24,71 @@ func Kary(file string, h int, k int, epoch int, threshold float64, alpha float64
 		go datastream.SyntheticDataStream(method, datapoints, dataChannel)
 	}
 
+	// List of all incoming packets during the epoch
 	packetList := make([]datastream.Packet, epoch)
+	//Total amount of anomalies detected
+	var anomCounter int
 
 	// Create the main sketch
 	s, err := sketch.New(h, k)
 	if err != nil {
 		log.Fatalln("error while creating sketch", err)
 	}
-
+	
 	// Forcasting variables
 	var (
-		forecastAlgo *forecasting.EWMA
+		forecastAlgo forecasting.Forecasting
 		fSketch      *sketch.Sketch
 	)
-	forecastAlgo = &forecasting.EWMA{Alpha: alpha}
+	
+	// Determine which forecasting algorithm to use
+	switch forecasting_algo {
+		case "ewma":
+			forecastAlgo = forecasting.NewEWMA(alpha)
+		case "velacc":
+			forecastAlgo = forecasting.NewVelAcc(epoch, s.H(), s.K())
+	}
 
 	// Variable representing how many times the algorithm has iterated
 	index := 0
 
 	for packet := range dataChannel {
+		// Check if the epoch has ended
 		if index%epoch == 0 && index > 0 {
 			fSketch, err = forecastAlgo.Forecast(s)
 			if err != nil {
 				log.Fatalln("error while creating new sketch", err)
 			}
 
+			// Go through all packets of the epoch to detect anomalies
 			for i, p := range packetList {
 				observedChange, thresholdChange, err := change.FEDetect(s, fSketch, threshold, p.ToBytes())
-				// If there is a change, save it in data.csv
+
+				// If there is an anomaly, save it in data.csv and print it to the terminal
 				if err != nil {
 					anomalies <- Data{index: index - epoch + i, packet: p, observedChange: observedChange, thresholdChange: thresholdChange}
-
 					fmt.Printf("Anomaly detected! Index: %d\t Observed change: %f, Threshold: %f\n", index-epoch+i, observedChange, thresholdChange)
 					anomCounter++
 				}
 			}
 
-			// Create new EWMA observed sketch for new epoch
-			s, err = sketch.New(h, k)
-			if err != nil {
-				log.Fatalln("error while performing sketch", err)
+			if forecasting_algo == "ewma" {
+				// Create new EWMA observed sketch for new epoch
+				s, err = sketch.New(h, k)
+				if err != nil {
+					log.Fatalln("error while performing sketch", err)
+				}
 			}
-
-			// Empty packet list to only check packets from new epoch
-			packetList = packetList[:0]
 		}
 
 		// Update sketch and forecasting
 		s.Update(packet.ToBytes(), 1)
-		packetList = append(packetList, packet)
-		//forecastAlgo.UpdateVelocity(s)
+		forecastAlgo.Update(s)
 
 		packetList[index % epoch] = packet
 
 		index++
 	}
-	fmt.Print(anomCounter)
 
+	fmt.Printf("Total number of anomalies detected: %d\n", anomCounter)
 }
